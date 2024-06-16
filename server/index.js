8724bc5e38845ec5aaf664e390d58e2f3b62dbef
Elizabeth Rodriguez Henry <elirhenry@Elizabeths-iMac.local>
2024-06-16 16:04:04 -0600

    knex

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => res.send('Hello there'))
+app.get('/', (req, res) => res.send('Why hello there'))
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
