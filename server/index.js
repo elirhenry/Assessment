12938df174469876900af996dbab69f049ffe42d
Elizabeth Rodriguez Henry <elirhenry@Elizabeths-iMac.local>
2024-06-16 14:03:24 -0600

    setup

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => res.send('Hello there'))
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
